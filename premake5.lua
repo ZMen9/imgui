7cc5901a456d56cca6fcfc9fcf4d3f78b9c401db
zhengzemeng1999@163.com <zhengzemeng1999@163.com>
2022-03-02 16:45:39 +0800

    modified premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -16,7 +16,8 @@ project "ImGui"
 		"imstb_rectpack.h",
 		"imstb_textedit.h",
 		"imstb_truetype.h",
-		"imgui_demo.cpp"
+		"imgui_demo.cpp",
+        "imgui_tables.cpp"
 	}
 
 	filter "system:windows"
@@ -25,10 +26,5 @@ project "ImGui"
 		staticruntime "On"
 
 
-	filter "configurations:Debug"
-		runtime "Debug"
-		symbols "on"
-
-	filter "configurations:Release"
-		runtime "Release"
-		optimize "on"
+	filter { "system:windows", "configurations:Realease" }
+		buildoptions "/MT"
