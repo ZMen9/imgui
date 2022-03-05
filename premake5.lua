72e31898e228ebf9d3dfcf1dab21b461d6d1b7b4
zhengzemeng1999@163.com <zhengzemeng1999@163.com>
2022-03-05 16:57:31 +0800

    updated lua file

--- premake5.lua
+++ premake5.lua
@@ -1,6 +1,7 @@
 project "ImGui"
 	kind "StaticLib"
 	language "C++"
+	cppdialect "C++17"
 
 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
 	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
@@ -20,11 +21,10 @@ project "ImGui"
         "imgui_tables.cpp"
 	}
 
-	filter "system:windows"
-		systemversion "latest"
-		cppdialect "C++17"
-		staticruntime "On"
+	filter "configurations:Debug"
+        runtime "Debug"
+        symbols "on"
 
-
-	filter { "system:windows", "configurations:Realease" }
-		buildoptions "/MT"
+    filter "configurations:Release"
+        runtime "Release"
+        optimize "on"
